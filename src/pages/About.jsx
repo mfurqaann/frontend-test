3eec38f81dd1c64bc510ca9f1e5bc9716cf7751e
mfurqaann <furqan1061@gmail.com>
2025-06-18 15:46:09 +0700

    refactor(smallap): refactor some code

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 const About = () => {
   return (
     <div className='flex justify-center items-center min-h-screen'>
-      About Page
+      <h1 className="text-3xl md:text-8xl font-bold text-blue-500">About Page</h1>
     </div>
   )
 }
